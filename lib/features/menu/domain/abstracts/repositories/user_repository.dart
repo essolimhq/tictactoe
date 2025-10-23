97e382632f16d8670f42f20eae7d74d10b36851c
Gildas <services@essolim.com>
2025-10-23 20:41:32 +0200

    initial commit

---
+++ lib/features/menu/domain/abstracts/repositories/user_repository.dart
@@ -0,0 +1,7 @@
+// import 'package:clean/features/menu/domain/entities/menu.dart';
+//
+// abstract class UserRepositoryInterface {
+//   Future<List<User>> getUsers();
+//   Future<User> getUserById(String id);
+//   Future<void> createUser(User menu);
+// }
